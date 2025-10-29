3ad220608f525d7710a826f88690770c8e2632b9
MikkieTee02 <mikaelakayz@gmail.com>
2025-10-29 23:31:05 +0200

    add order model import and fix maxSize formatting

--- config/inngest.js
+++ config/inngest.js
@@ -1,6 +1,7 @@
 import { Inngest } from "inngest";
 import connectDB from "./db";
 import User from "@/models/User";
+import Order from "@/models/Order";
 
 // Create a client to send and receive events
 export const inngest = new Inngest({ id: "nutrizo" });
@@ -78,7 +79,7 @@ export const createUserOrder = inngest.createFunction(
     {
         id: 'create-user-order',
         batchEvents:{
-            maxSize:5,
+            maxSize: 5,
             timeout: '5s'
         }
 
