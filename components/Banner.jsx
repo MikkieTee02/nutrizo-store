41b75cf0115c6754ee443ac843fa38bc97c44c57
MikkieTee02 <mikaelakayz@gmail.com>
2025-11-06 10:24:48 +0200

    enhance banner with decorative shapes and improve layout

--- components/Banner.jsx
+++ components/Banner.jsx
@@ -5,41 +5,59 @@ import Image from "next/image";
 const Banner = () => {
   return (
     <div className="flex flex-col md:flex-row items-center justify-between  py-14 md:py-0 bg-green-100 my-16  overflow-hidden">
-    
-       <Image
-        className="w-56 lg:w-[50%] h-full"
-        src={assets.jbl_soundbox_image}
-        alt="jbl_soundbox_image"
-      />
-      
-      
-        
+      <div className=" w-56 lg:w-[50%] h-full relative ">
+        <Image
+          className="  relative w-56 lg:w-full h-full z-40 "
+          src={assets.jbl_soundbox_image}
+          alt="jbl_soundbox_image"
+        />
 
-      
-    
+        <img
+          src="https://res.cloudinary.com/dmlavu7is/image/upload/v1762413944/shape_gi2z08.png"
+          alt="Bitcoin"
+          className=" hidden md:block lg:w-[80%] z-10 lg:z-0 object-cover absolute top-0 "
+        />
+      </div>
+
       <div className="flex flex-col items-center justify-center text-center space-y-2 px-4 md:px-0">
         <h2 className="text-2xl md:text-3xl font-semibold max-w-[290px]">
-         Organic Only
+          Organic Only
         </h2>
         <p className="max-w-[450px] font-medium text-2xl text-text-secondary">
-        We supply hight quality organic product
+          We supply hight quality organic product
         </p>
 
         <p className="max-w-[343px]  text-text-secondary">
-         Eius causae omittantur nec id, consulatu reformidans at nam, deleniti inciderint intellegebat ex ius. Ne delectus voluptaria vituperata qui. In elitr congue est, in nam quis brute menandri.
+          Eius causae omittantur nec id, consulatu reformidans at nam, deleniti
+          inciderint intellegebat ex ius. Ne delectus voluptaria vituperata qui.
+          In elitr congue est, in nam quis brute menandri.
         </p>
         <button className="group flex items-center justify-center gap-1 px-12 py-2.5 bg-accent rounded-lg text-white font-semibold">
           Buy now
-          <Image className="group-hover:translate-x-1 transition" src={assets.arrow_icon_white} alt="arrow_icon_white" />
+          <Image
+            className="group-hover:translate-x-1 transition"
+            src={assets.arrow_icon_white}
+            alt="arrow_icon_white"
+          />
         </button>
       </div>
+
+      <div className=" w-56 lg:w-[50%] h-full relative ">
+        <Image
+          className="  relative w-56 lg:w-full h-full z-40 "
+          src={assets.md_controller_image}
+          alt="md_controller_image"
+        />
+
+        <img
+          src="https://res.cloudinary.com/dmlavu7is/image/upload/v1762413942/shape-pink_lshc7w.png"
+          alt="Bitcoin"
+          className=" hidden md:block lg:w-[80%] z-10 lg:z-0 object-cover absolute top-0 lg:right-0 "
+        />
+      </div>
+
       <Image
-        className="hidden md:block w-56 lg:w-[50%] h-full"
-        src={assets.md_controller_image}
-        alt="md_controller_image"
-      />
-      <Image
-        className="md:hidden"
+        className="hidden"
         src={assets.sm_controller_image}
         alt="sm_controller_image"
       />
--- components/NewsLetter.jsx
+++ components/NewsLetter.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const NewsLetter = () => {
   return (
     <div className="flex flex-col items-center justify-center text-center space-y-2 pt-8 pb-14">
-      <div className="text-center mb-12">
+      <div className="text-center px-2 mb-12">
         <h2 className="text-3xl font-bold text-text-primary">Subscribe now & get 20% off</h2>
         <div className="flex justify-center mt-2">
           <div className="w-24 h-1 bg-primary"></div>
@@ -13,7 +13,7 @@ const NewsLetter = () => {
         Lorem Ipsum is simply dummy text of the printing and typesetting
         industry.
       </p>
-      <div className="flex items-center justify-between max-w-2xl w-full md:h-14 h-12">
+      <div className="flex items-center justify-between max-w-2xl w-full px-2  md:h-14 h-12">
         <input
           className="border border-border-color rounded-md h-full border-r-0 outline-none w-full rounded-r-none px-3 text-text-secondary"
           type="text"
